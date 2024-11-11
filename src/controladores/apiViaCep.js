cddfbee3bb5aa4285bae7dca43beaa2a8f700d73
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-11-11 14:37:03 -0300

    criação de consulta via api cep

---
+++ src/controladores/apiViaCep.js
@@ -0,0 +1,28 @@
+const viaCepApi = (req, res) => {
+    let dadosApi;
+    const {
+        cep
+    } = req;
+
+    try {
+        const url = `viacep.com.br/ws/${cep}/json/`;
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+
+                let dadosApi = data;
+
+            })
+            .catch(error => {
+                console.error('Erro ao consultar o CEP:', error.message);
+            });
+
+        res.status(200).json(dadosApi);
+
+    } catch (error) {
+        res.status(500).json({ mensagem: `${error.message}` });
+    }
+
+}
+
+module.exports = viaCepApi;
