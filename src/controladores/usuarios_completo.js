e71646ad0052beb4f13882dd2147cb26103d82a9
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-11-19 10:30:41 -0300

    ajuste da mensagem no cad usuário completo

--- src/controladores/usuarios_completo.js
+++ src/controladores/usuarios_completo.js
@@ -91,7 +91,7 @@ const cadastrarUsuario = async (req, res) => {
         }
 
         
-        return res.status(201).json(rows[0]); 
+        return res.status(201).json({}); 
 
     } catch (error) {
         return res.status(500).json({ mensagem: `${error.message}` });
