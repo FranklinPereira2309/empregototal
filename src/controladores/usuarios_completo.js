74624d84b0571c323bd3887785cf9004571de610
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-12-12 00:27:00 -0300

    correção da mensagem de dados inexistente em dados completos usuário

--- src/controladores/usuarios_completo.js
+++ src/controladores/usuarios_completo.js
@@ -114,11 +114,12 @@ const consultarUsuario = async (req, res) => {
 
         const { rows, rowCount} = await conexao.query(dadosUsuario, [usuario.id]);
 
-        if(rowCount === 0) {
-            return res.status(404).json({mensagem:'Não há Dados Cadastrados!'});
-        }
+        // if(rowCount === 0) {
+        //     return res.status(404).json({mensagem:'Não há Dados Cadastrados!'});
+        // }        
         
         return res.status(200).json(rows[0]);
+        
 
     } catch (error) {
         return res.status(500).json({ mensagem: `${error.message}`});
